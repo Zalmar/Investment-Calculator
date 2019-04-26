d92c02f5ac4dac94dd008adb405c58ff6b9ef97a
Zalmar <zalmardev@gmail.com>
2019-04-26 18:34:06 +0400

    Add fonts

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
 function getValues() {
-  let deposit = document.getElementById("money").value
-  let term = document.getElementById("mounths").value
+
+  let deposit = parseInt(document.getElementById("money").value)
+  let term = parseInt(document.getElementById("mounths").value)
 
   let radioBtns = document.getElementsByName("radio-buttons")
   for(let i = 0; i < radioBtns.length; i++){
     if(radioBtns[i].checked){
       checkBtn = radioBtns[i].value;
-      document.getElementById("pros").innerHTML = checkBtn;
+      document.getElementById("pers").innerHTML = checkBtn;
     }
   }
   
-  document.getElementById("rangeMoney").innerHTML = document.getElementById("money").value;
-  document.getElementById("rangeMonths").innerHTML = document.getElementById("mounths").value;
-
+  document.getElementById("rangeMoney").innerHTML = deposit.toLocaleString();
+  document.getElementById("rangeMonths").innerHTML = term;
+  
+  //расчет суммы профита 
   let leftProfit = calcProfit(deposit, 6.5, term);
   let rightProfit = calcProfit(deposit, checkBtn, term);
-  document.getElementById("left-profit").innerHTML = leftProfit
-  document.getElementById("right-profit").innerHTML = rightProfit;
-  
-  let cylinderHeigth = ((rightProfit-leftProfit)/rightProfit) 
-  let cylinder = document.getElementById("lc");
-  cylinder.style.height = 220 - (200 * cylinderHeigth)  + "px";
-  cylinder.style.transitionDuration = "0.5s";
 
+  document.getElementById("left-profit").innerHTML = leftProfit.toLocaleString();
+  document.getElementById("right-profit").innerHTML = rightProfit.toLocaleString();
+
+  // получение столбцов
+  let leftCylinder = document.getElementById("lc");
+  let rightCylinder = document.getElementById("rc");
+
+  let delta = (rightProfit - leftProfit) / leftProfit  
+  console.log(delta)
+
+  leftCylinder.style.height = 199 - (199 * delta) + 'px';
+  leftCylinder.style.transitionDuration = "0.5s";
 
   let moneySlider = document.getElementById("money");
   moneySlider.style.background = 'linear-gradient(to right, #4bd1a0 0%, #4bd1a0 '+moneySlider.value / 100000 +'%, #e6e6e6 ' + moneySlider.value / 100000 + '%, #e6e6e6 100%)'
@@ -30,8 +37,8 @@ function getValues() {
   mounthsSlider.style.background = 'linear-gradient(to right, #4bd1a0 0%, #4bd1a0 '+ mounthsSlider.value * 1.6 +'%, #e6e6e6 ' + mounthsSlider.value * 1.6 + '%, #e6e6e6 100%)'
 };
 
-function calcProfit (deposit, pros, term) {
-  return Math.round(deposit * (1 + ((pros/100) / 12)) ** term) - deposit
+function calcProfit (deposit, pers, term) {
+  return Math.round(deposit * (1 + (pers / 100) / 12) ** term) - deposit
 }
 
 getValues();

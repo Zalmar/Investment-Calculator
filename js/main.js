610a5de9d03a76e72cf5dedd520ab02588c98064
Zalmar <zalmardev@gmail.com>
2019-04-28 15:57:14 +0400

    Code refactoring

--- js/main.js
+++ js/main.js
@@ -1,68 +1,95 @@
 function getValues() {
-
-  let deposit = parseInt(document.getElementById("money").value);
-  let term = parseInt(document.getElementById("mounths").value);
-  
-  let radioBtns = document.getElementsByName("radio-buttons")
-  for(let i = 0; i < radioBtns.length; i++){
-    if(radioBtns[i].checked){
-      checkBtn = radioBtns[i].value;
-      document.getElementById("pers").innerHTML = checkBtn;
+    // get the deposit and term from input range
+    let deposit = parseInt(document.getElementById("money").value);
+    let term = parseInt(document.getElementById("mounths").value);
+    
+    //get the percent from radio buttons
+    let radioBtns = document.getElementsByName("radio-buttons");
+    for(let i = 0; i < radioBtns.length; i++){
+        if(radioBtns[i].checked){
+            checkBtn = radioBtns[i].value;
+            document.getElementById("perc").innerHTML = checkBtn;
+        }
     }
-  }
-  
-  document.getElementById("rangeMoney").innerHTML = deposit.toLocaleString();
-  document.getElementById("rangeMonths").innerHTML = term;
 
-  document.getElementById("modalSumma").innerHTML = deposit.toLocaleString();
-  document.getElementById("modalTerm").innerHTML = term.toLocaleString();
-  document.getElementById("modalPercent").innerHTML = checkBtn;
+    // set deposit and term in <span>
+    document.getElementById("rangeMoney").innerHTML = deposit.toLocaleString();
+    document.getElementById("rangeMounths").innerHTML = term;
+    
+    // set values in of the modal window
+    document.getElementById("modalSumma").innerHTML = deposit.toLocaleString();
+    document.getElementById("modalTerm").innerHTML = term.toLocaleString();
+    document.getElementById("modalPercent").innerHTML = checkBtn;
 
-  let leftProfit = calcProfit(deposit, 6.5, term);
-  let rightProfit = calcProfit(deposit, checkBtn, term);
+    // calculation profit 
+    let leftProfit = calcProfit(deposit, 6.5, term);
+    let rightProfit = calcProfit(deposit, checkBtn, term);
 
-  document.getElementById("left-profit").innerHTML = leftProfit.toLocaleString();
-  document.getElementById("right-profit").innerHTML = rightProfit.toLocaleString();
+    // set profit values
+    document.getElementById("leftProfit").innerHTML = leftProfit.toLocaleString();
+    document.getElementById("rightProfit").innerHTML = rightProfit.toLocaleString();
 
-  // получение столбцов
-  let leftCylinder = document.getElementById("lc");
-  let rightCylinder = document.getElementById("rc");
+    // get elememt cylinders elememt
+    let leftCylinder = document.getElementById("leftCylinder");
+    let rightCylinder = document.getElementById("rightCylinder");
 
-  let element = document.getElementById('lc'),
-  style = window.getComputedStyle(element),
-  max_height = style.getPropertyValue('max-height').slice(0,3);
-  max_height = max_height - 42.5
-  
-  max_h_bottom = style.getPropertyValue('max-height').slice(0,2);
+    // get maximum height cylinder
+    let element = document.getElementById('leftCylinder');
+    style = window.getComputedStyle(element);
+    max_height = style.getPropertyValue('max-height').slice(0,3);
 
-  let deltaProfit = (rightProfit - leftProfit) / rightProfit
-  let leftCylinderHeight = max_height - ((max_height * deltaProfit))
+    // cylinder bottom
+    let bottomHeight;
+    if (max_height > 110) {
+        bottomHeight = 50;
+    } else {
+        bottomHeight = 30;
+    }
 
-  let deltaTerm = (60 - term) / 60
-  leftCylinder.style.height = leftCylinderHeight - (leftCylinderHeight * deltaTerm) + 46  + 'px';
-  leftCylinder.style.transitionDuration = "0.5s";
+    // transform height
+    cylinderHeight = max_height - bottomHeight;
 
-  rightCylinder.style.height = max_height - (max_height * deltaTerm) + 50 + 'px'
-  rightCylinder.style.transitionDuration = "0.5s";
+    // calculation ratio profit
+    let ratioProfit = (rightProfit - leftProfit) / rightProfit;
+    let leftCylinderHeight = cylinderHeight - ((cylinderHeight * ratioProfit));
+    
+    // ratio term calculation. Cylinder height depends on the term
+    // let ratioTerm = (60 - term) / 60;
 
-  let moneySlider = document.getElementById("money");
-  moneySlider.style.background = 'linear-gradient(to right, #4bd1a0 0%, #4bd1a0 '+moneySlider.value / 100000 +'%, #e6e6e6 ' + moneySlider.value / 100000 + '%, #e6e6e6 100%)'
-  let mounthsSlider = document.getElementById("mounths");
-  mounthsSlider.style.background = 'linear-gradient(to right, #4bd1a0 0%, #4bd1a0 '+ mounthsSlider.value * 1.6 +'%, #e6e6e6 ' + mounthsSlider.value * 1.6 + '%, #e6e6e6 100%)'
-};
+    // ratio deposit calculation. Cylinder height depends on the amount of the deposit
+    let ratioTerm = (10000000 - deposit) / 10000000;
 
-function calcProfit (deposit, pers, term) {
-  return Math.round(deposit * (1 + (pers / 100) / 12) ** term) - deposit
+    // set the height cylinders and duration animations
+    leftCylinder.style.height = leftCylinderHeight - (leftCylinderHeight * ratioTerm) + bottomHeight  + 'px';
+    leftCylinder.style.transitionDuration = "0.5s";
+
+    rightCylinder.style.height = cylinderHeight - (cylinderHeight * ratioTerm) + bottomHeight + 5 + 'px';
+    rightCylinder.style.transitionDuration = "0.5s";
+    
+    // input range progress bar
+    let moneySlider = document.getElementById("money");
+    moneySlider.style.background = 'linear-gradient(to right, #4bd1a0 0%, #4bd1a0 '+moneySlider.value / 100000 +'%, #e6e6e6 ' + moneySlider.value / 100000 + '%, #e6e6e6 100%)';
+    
+    let mounthsSlider = document.getElementById("mounths");
+    mounthsSlider.style.background = 'linear-gradient(to right, #4bd1a0 0%, #4bd1a0 '+ mounthsSlider.value * 1.6 +'%, #e6e6e6 ' + mounthsSlider.value * 1.6 + '%, #e6e6e6 100%)';
+}
+
+function calcProfit (deposit, perc, term) {
+    // calculation deposits with monthly capitalization
+    return Math.round(deposit * (1 + (perc / 100) / 12) ** term) - deposit;
 }
 
 function popupShow() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+    // show popup window
+    let popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
 }
 
-function popupRemove(){
-  var popup = document.getElementById("myPopup");
-  popup.classList.remove("show")
+function popupHide(){
+    // hide popup window
+    let popup = document.getElementById("myPopup");
+    popup.classList.remove("show");
 }
 
+// call function in start
 getValues();
